74ef68a412c316ae39368076db110c5b6fb6f3f5
Your Name <you@example.com>
2023-05-27 09:51:22 +0700

    Add project files.

---
+++ Variabel_dan_tipe_Data/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variabel_dan_tipe_Data
+	//Variabel adalah tempat untuk menyimpan nilai/data
+	//Tipe data adalah jenis data (int,char)
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//int A = 5; //int : Tipe Data, A : Variabel , 5: Nilai
+			//int B = A + 5; // 5 + 5 ;
+
+			//char Wahyu = 'A'; // nilai di beri tanda petik '?'
+
+			//string WahyuNurdian = "Junior Fullstak"; //Nilai di beri petik 2 "?"
+			//string WahyuNurdian2 = WahyuNurdian + "Developer"; 
+
+			//bool keadaan = true; //true/false
+			Console.WriteLine(keadaan); //Menampilkan nilai (?)
+		}
+	}
+}
